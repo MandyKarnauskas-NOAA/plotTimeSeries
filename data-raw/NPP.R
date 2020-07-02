aa117abad6a22e49601a38ff1423e991c0ae1074
Mandy Karnauskas <mandy.karnauskas@noaa.gov>
2020-07-02 12:03:37 -0400

    adding data files to package attempt 2

--- data-raw/NPP.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `NPP` dataset goes here
-
-usethis::use_data(NPP, overwrite = TRUE)
--- data-raw/menhaden.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `menhaden` dataset goes here
-
-usethis::use_data(menhaden, overwrite = TRUE)
